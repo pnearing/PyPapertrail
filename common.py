bbd2fc81ebf425f8fdc05eedb5586cbd80da31ba
Peter Nearing <me@peternearing.ca>
2023-08-20 19:05:20 -0400

    add __type_error__

--- common.py
+++ common.py
@@ -2,6 +2,7 @@
 """
 Common variables / functions for papertrail api.
 """
+from typing import Any, NoReturn
 from datetime import datetime
 
 BASE_URL: str = 'https://papertrailapp.com/api/v1/'
@@ -15,3 +16,14 @@ def is_timezone_aware(dt: datetime) -> bool:
     """
     #
     return dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None
+
+
+def __type_error__(desired_types: str, received_obj: Any) -> NoReturn:
+    """
+    Raise a TypeError with a good message.
+    :param desired_types: Str: String of desired type(s).
+    :param received_obj: The var which was received, note: type() will be called on it.
+    :return: NoReturn
+    """
+    error: str = "TypeError: got %s type, expected: %s" % (str(type(received_obj)), desired_types)
+    raise TypeError(error)
