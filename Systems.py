43470da6d8e79dffaad392e361b7db381c20610f
Peter Nearing <me@peternearing.ca>
2023-08-20 21:59:05 -0400

    rearrange, type checks and commenting.

--- Systems.py
+++ Systems.py
@@ -2,42 +2,59 @@
 from typing import Optional, Iterator
 from datetime import datetime, timezone
 import requests
-from common import BASE_URL
+from common import BASE_URL, __type_error__
 from Exceptions import SystemsError
+from Destinations import Destination
 
 
 class System(object):
     """Class to store a single system."""
 
+################################
+# Initialize:
+################################
     def __init__(self,
                  api_key: str,
+                 last_fetched: Optional[datetime] = None,
                  raw_system: Optional[dict] = None,
                  from_dict: Optional[dict] = None,
                  ) -> None:
         """
         Initialize a System object:
         :param api_key: Str: The api key.
+        :param last_fetched: Optional[datetime]: The datetime object for when this was last fetched, note: must be set
+            if using the raw_system parameter, and it will be ignored if using from_dict parameter.
         :param raw_system: Dict: The dict received from papertrail.
         :param from_dict: Dict: The dict created by __to_dict__().
         :raises: SystemsError: If raw_system and from_dict are either both None or both defined, or if an invalid
             raw_system dict, or from_dict dict are lacking a key.
+        :raises: TypeError: If an invalid type is passed as a parameter.
         :returns: None
         """
+        # Type checks:
+        if not isinstance(api_key, str):
+            __type_error__("api_key", "str", api_key)
+        elif last_fetched is not None and not isinstance(last_fetched, datetime):
+            __type_error__("last_fetched", "datetime", last_fetched)
+        elif raw_system is not None and not isinstance(raw_system, dict):
+            __type_error__("raw_system", "dict", raw_system)
+        elif from_dict is not None and not isinstance(from_dict, dict):
+            __type_error__("from_dict", "dict", from_dict)
         # Store the api key.
         self._api_key: str = api_key
         # Define the properties:
-        self._id: int
-        self._name: str
-        self._last_event: Optional[datetime]
-        self._auto_delete: bool
-        self._json_info_link: str
-        self._html_info_link: str
-        self._search_link: str
-        self._ip_address: Optional[str]
-        self._host_name: Optional[str]
-        self._syslog_host_name: str
-        self._syslog_port: int
-        self._last_fetched: datetime
+        self._id: int = -1
+        self._name: str = ''
+        self._last_event: Optional[datetime] = None
+        self._auto_delete: bool = False
+        self._json_info_link: str = ''
+        self._html_info_link: str = ''
+        self._search_link: str = ''
+        self._ip_address: Optional[str] = None
+        self._host_name: Optional[str] = None
+        self._syslog_host_name: str = ''
+        self._syslog_port: int = -1
+        self._last_fetched: datetime = last_fetched
         # Load from raw_system, or from_dict:
         if raw_system is None and from_dict is None:
             error: str = "Either raw_system must be defined, but not both."
@@ -52,6 +69,9 @@ class System(object):
             self.__from_dict__(from_dict)
         return
 
+##################################
+# Load / Save functions:
+##################################
     def __from_raw_system__(self, raw_system: dict) -> None:
         """
         Load from a raw system dict provided by papertrail.
@@ -130,6 +150,66 @@ class System(object):
             return_dict['last_event'] = self._last_event.isoformat()
         return return_dict
 
+#############################################
+# Methods:
+#############################################
+    def reload(self, raise_on_error: bool = True) -> tuple[bool, str]:
+        """
+        Reload data from papertrail.
+        :param raise_on_error: Bool: True, raise SystemsError when an error occurs, False, return False on error.
+        :raises: SystemsError: When a request error or a JSON error occurs.
+        :return: Tuple[bool, str]: The first element, the bool, is True upon success, and False upon failure.
+            If the first element is True, the second element will be the message "OK", otherwise if False, the second
+            element will be a message describing the error.
+        """
+        # Type checks:
+        if not isinstance(raise_on_error, bool):
+            __type_error__("raise_on_error", "bool", raise_on_error)
+        # Build url and headers:
+        url = self._json_info_link
+        headers = {'X-Papertrail-Token': self._api_key}
+        # Request the data from papertrail:
+        try:
+            r = requests.get(url, headers=headers)
+        except requests.ReadTimeout as e:
+            error: str = "ReadTimeout: error_num=%i, strerror=%s" % (e.errno, e.strerror)
+            if raise_on_error:
+                raise SystemsError(error, exception=e)
+            else:
+                return False, error
+        # Check request status:
+        try:
+            r.raise_for_status()
+        except requests.HTTPError as e:
+            error: str = "HTTPError: error_num=%i, strerror=%s" % (e.errno, e.strerror)
+            if raise_on_error:
+                raise SystemsError(error, exception=e, request=r)
+            else:
+                return False, error
+        except requests.RequestException as e:
+            error: str = "RequestException: error_num=%i, strerror=%s" % (e.errno, e.strerror)
+            if raise_on_error:
+                raise SystemsError(error, exception=e, request=r)
+            else:
+                return False, error
+        # Parse JSON response:
+        try:
+            raw_system: dict = r.json()
+        except requests.JSONDecodeError as e:
+            error = "Server sent bad JSON: error_num=%i, strerror=%s" % (e.errno, e.strerror)
+            if raise_on_error:
+                raise SystemsError(error, exception=e, request=r)
+            else:
+                return False, error
+        self.__from_raw_system__(raw_system)
+        return True, "OK"
+
+    def update(self, raise_on_error: bool = True) -> tuple[bool, str]:
+        pass
+
+#########################################
+# Properties:
+#########################################
     @property
     def id(self) -> int:
         """
@@ -226,53 +306,8 @@ class System(object):
         :return: Timezone-aware datetime object.
         """
         return self._last_fetched
-
-    def reload(self, raise_on_error: bool = True) -> tuple[bool, str]:
-        """
-        Reload data from papertrail.
-        :param raise_on_error: Bool: True, raise SystemsError when an error occurs, False, return False on error.
-        :raises: SystemsError: When a request error or a JSON error occurs.
-        :return: Tuple[bool, str]: The first element, the bool, is True upon success, and False upon failure.
-            If the first element is True, the second element will be the message "OK", otherwise if False, the second
-            element will be a message describing the error.
-        """
-        url = self._json_info_link
-        headers = {'X-Papertrail-Token': self._api_key}
-        # Request the data from papertrail:
-        try:
-            r = requests.get(url, headers=headers)
-        except requests.ReadTimeout as e:
-            error: str = "ReadTimeout: error_num=%i, strerror=%s" % (e.errno, e.strerror)
-            if raise_on_error:
-                raise SystemsError(error, exception=e)
-            else:
-                return False, error
-        # Check request status:
-        try:
-            r.raise_for_status()
-        except requests.HTTPError as e:
-            error: str = "HTTPError: error_num=%i, strerror=%s" % (e.errno, e.strerror)
-            if raise_on_error:
-                raise SystemsError(error, exception=e, request=r)
-            else:
-                return False, error
-        except requests.RequestException as e:
-            error: str = "RequestException: error_num=%i, strerror=%s" % (e.errno, e.strerror)
-            if raise_on_error:
-                raise SystemsError(error, exception=e, request=r)
-            else:
-                return False, error
-        # Parse JSON response:
-        try:
-            raw_system: dict = r.json()
-        except requests.JSONDecodeError as e:
-            error = "Server sent bad JSON: error_num=%i, strerror=%s" % (e.errno, e.strerror)
-            if raise_on_error:
-                raise SystemsError(error, exception=e, request=r)
-            else:
-                return False, error
-        self.__from_raw_system__(raw_system)
-        return True, "OK"
+########################################################################################################################
+########################################################################################################################
 
 
 class Systems(object):
@@ -281,6 +316,9 @@ class Systems(object):
     _IS_LOADED: bool = False
     _LAST_FETCHED: Optional[datetime] = None
 
+###############################
+# Initialize:
+###############################
     def __init__(self,
                  api_key: str,
                  from_dict: Optional[dict] = None,
@@ -293,13 +331,25 @@ class Systems(object):
         :param do_load: Bool: True, make request from papertrail, False do not.
         :raises: SystemsError: On request error, or if invalid JSON is returned.
         """
+        # Type Checks:
+        if not isinstance(api_key, str):
+            __type_error__("api_key", "str", api_key)
+        elif from_dict is not None and not isinstance(from_dict, dict):
+            __type_error__("from_dict", "dict", from_dict)
+        elif not isinstance(do_load, bool):
+            __type_error__("do_load", "bool", do_load)
+        # store api key:
         self._api_key: str = api_key
+        # Load Systems:
         if from_dict is not None:
             self.__from_dict__(from_dict)
         elif do_load:
             self.load()
         return
 
+##############################################
+# Loading / saving functions:
+##############################################
     def __to_dict__(self) -> dict:
         """
         Create a json / pickle friendly dict.
@@ -331,23 +381,13 @@ class Systems(object):
         self._IS_LOADED = True
         return
 
-    @property
-    def last_fetched(self) -> Optional[datetime]:
-        """
-        When the systems were last fetched from papertrail.
-        :return: Optional[datetime]
-        """
-        return self._LAST_FETCHED
-
-    @property
-    def is_loaded(self) -> bool:
-        """
-        Has the systems list been loaded?
-        :return: Bool
-        """
-        return self._IS_LOADED
-
+###############################
+# Methods:
+###############################
     def load(self, raise_on_error: bool = True) -> tuple[bool, str]:
+        # Type checks:
+        if not isinstance(raise_on_error, bool):
+            __type_error__("raise_on_error", "bool", raise_on_error)
         # Set url and headers:
         url = BASE_URL + 'systems.json'
         headers = {'X-Papertrail-Token': self._api_key}
@@ -384,14 +424,54 @@ class Systems(object):
                 raise SystemsError(error, exception=e, request=r)
             else:
                 return False, error
+        # Set last fetched time to NOW.
+        self._LAST_FETCHED = datetime.utcnow().replace(tzinfo=timezone.utc)
         # Create SYSTEMS list:
         for raw_system in system_list:
-            system = System(self._api_key, raw_system)
+            system = System(api_key=self._api_key, last_fetched=self._LAST_FETCHED, raw_system=raw_system)
             self._SYSTEMS.append(system)
         self._IS_LOADED = True
-        self._LAST_FETCHED = datetime.utcnow().replace(tzinfo=timezone.utc)
         return True, "OK"
 
+###################################################
+# Methods:
+###################################################
+    def register(self,
+                 name: str,
+                 host_name: str,
+                 destination_port: Optional[int] = None,
+                 destination_id: Optional[int] = None,
+                 destination: Optional[Destination] = None,
+                 description: Optional[str] = None,
+                 auto_delete: Optional[bool] = None,
+                 raise_on_error: bool = True,
+                 ) -> tuple[bool, str]:
+        # Type checks:
+        if not isinstance(name, str):
+            __type_error__("name", "str", name)
+        elif len(name) == 0:
+            raise ValueError("name must not be of 0 length.")
+        elif not isinstance(host_name, str):
+            __type_error__("host_name", "str", host_name)
+        elif len(host_name) == 0:
+            raise ValueError("host_name must not be of 0 length.")
+        elif destination_port is not None and not isinstance(destination_port, int):
+            __type_error__("destination_port", "int", destination_port)
+        elif destination_id is not None and not isinstance(destination_id, int):
+            __type_error__("destination_id", "int", destination_id)
+        elif destination is not None and not isinstance(destination, Destination):
+            __type_error__("destination", "Destination", destination)
+        elif description is not None and not isinstance(description, str):
+            __type_error__("description", "str", description)
+        elif auto_delete is not None and not isinstance(auto_delete, bool):
+            __type_error__("auto_delete", "bool", auto_delete)
+        elif not isinstance(raise_on_error, bool):
+            __type_error__("raise_on_error", "bool", raise_on_error)
+        return
+
+######################################################
+# List like overrides:
+######################################################
     def __getitem__(self, item) -> System | list[System]:
         """
         Index systems.
@@ -423,6 +503,33 @@ class Systems(object):
         :return: Iterator
         """
         return iter(self._SYSTEMS)
+
+    def __len__(self) -> int:
+        """
+        Return the number of systems.
+        :return: Int
+        """
+        return len(self._SYSTEMS)
+
+###################################################
+# Properties:
+###################################################
+
+    @property
+    def last_fetched(self) -> Optional[datetime]:
+        """
+        When the systems were last fetched from papertrail.
+        :return: Optional[datetime]
+        """
+        return self._LAST_FETCHED
+
+    @property
+    def is_loaded(self) -> bool:
+        """
+        Has the systems list been loaded?
+        :return: Bool
+        """
+        return self._IS_LOADED
 
 
 if __name__ == '__main__':
