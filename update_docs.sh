f3113f1ae6fb9e02841b23535a3439aa3d171612
Peter Nearing <me@peternearing.ca>
2023-09-18 07:30:15 -0400

    add update_docs.sh to update docs.

---
+++ update_docs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf docs/html
+pdoc --footer-text 'Version 1.7' --favicon 'https://images.peternearing.ca/PyPapertrail_icon.ico' --logo 'https://images.peternearing.ca/PyPapertrail_logo.png' -o docs/html/ src/PyPapertrail/*.py
