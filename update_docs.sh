a24490a8597938819a8c95db8f75f9e2c4c3e28b
Peter Nearing <me@peternearing.ca>
2023-09-18 07:48:52 -0400

    Add output to update_docs.sh

--- update_docs.sh
+++ update_docs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "Removing docs/html/"
 rm -rf docs/html
-
+echo "Creating documentation..."
 pdoc --footer-text 'Version 1.7' \
 --favicon 'https://images.peternearing.ca/PyPapertrail_icon.ico' \
 --logo 'https://images.peternearing.ca/PyPapertrail_logo.png' \
 --output-directory 'docs/html/' \
 src/PyPapertrail/*.py
+echo "Complete."
